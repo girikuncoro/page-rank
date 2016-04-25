51cec04eb7dd51ceccf2d65f44324645e67917f7
girikuncoro <girikuncoro@gmail.com>
2016-04-25 04:49:15 -0400

    Completed BlockedPageRank runner

--- src/proj2/main/blocked/BlockedPageRank.java
+++ src/proj2/main/blocked/BlockedPageRank.java
@@ -1,5 +1,66 @@
 package proj2.main.blocked;
 
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import proj2.main.util.Constants;
+
+/**
+ * Runner for Blocked Computation Page Rank
+ * MapReduce jobs will be run until convergence
+ * It shouldn't take than more than 7 passes to converge, faster than SimplePageRank
+ */
 public class BlockedPageRank {
 
+	public static void main(String[] args) throws IllegalArgumentException, IOException, ClassNotFoundException, InterruptedException{
+		if (args.length != 2){
+			throw new IllegalArgumentException("Please specify the input and output path"); 
+		}
+
+		String inputPath = args[0];
+		String outputPath = args[1];
+		
+		int passNum = 0;
+		double avgResidual = Double.MAX_VALUE;
+		
+		// run map reduce pass until convergence
+		do {
+			// create a new job, with unique jobname
+			Configuration conf = new Configuration();
+			Job job = Job.getInstance(conf, "BlockedPageRank " + passNum);
+
+			// specify various job-specific parameters
+			job.setJarByClass(BlockedPageRank.class);
+			job.setMapperClass(BlockedMapper.class);
+			job.setReducerClass(BlockedReducer.class);
+			job.setOutputKeyClass(Text.class);
+			job.setOutputValueClass(Text.class);
+			
+			// use preprocessed input file in the first pass
+			// but use output of previous job if not
+			if (passNum == 0){
+			    FileInputFormat.addInputPath(job, new Path(inputPath));
+		    } else{
+			    FileInputFormat.addInputPath(job, new Path(outputPath + "iteration" + (passNum-1)));
+		    }
+			FileOutputFormat.setOutputPath(job, new Path(outputPath + "/iteration" + passNum));
+			
+			// wait current job to finish before starts next pass
+			job.waitForCompletion(true);
+			
+		    avgResidual = (double)job.getCounters().findCounter(Constants.BlockedCounterEnum.BLOCKED_RESIDUAL).getValue() / Constants.PRECISION_FACTOR;
+		    avgResidual /= Constants.NODE_NUM;
+		    
+		    //Iteration 0 avg error 2.332958e+00
+		    System.out.println("Iteration " + passNum + " average error " + avgResidual);
+		    passNum++;
+			
+		} while (avgResidual > Constants.CONVERGENCE);
+	}
 }
